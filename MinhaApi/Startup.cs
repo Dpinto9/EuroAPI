9bc6c55b93c01308e03cf96f9411192eebccbfae
Diogo Pinto <113483856+Dpinto9@users.noreply.github.com>
2024-07-15 19:34:27 +0100

    Server side

---
+++ MinhaApi/Startup.cs
@@ -0,0 +1,65 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace MinhaApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext <MyDbContext> (options =>
+                options.UseMySQL(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddControllers();
+
+            // Configure JWT Authentication
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = Configuration["Jwt:Issuer"],
+                    ValidAudience = Configuration["Jwt:Audience"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["Jwt:Key"]))
+                };
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
---
+++ MinhaApi/User.cs
@@ -0,0 +1,21 @@
+﻿namespace MinhaApi
+{
+    public class Conta
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string? Role { get; set; }
+
+    }
+
+    public class Estadio
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public int Capacidade { get; set; }
+        public string Morada { get; set; }
+        public string Cidade { get; set; }
+    }
+
+}
